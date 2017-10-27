9baa89e48e2d97eb6e12a86874b32be7df24c1de
巫瑜通 <1019281845@qq.com>
2017-10-27 19:19:45 +0800

    no message

---
+++ android/app/src/main/java/com/wyatt/myblog/CreateBlogActivity.kt
@@ -0,0 +1,12 @@
+package com.wyatt.myblog
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateBlogActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_blog)
+    }
+}
--- android/app/src/main/java/com/wyatt/myblog/MainActivity.java
+++ android/app/src/main/java/com/wyatt/myblog/MainActivity.java
@@ -1,13 +1,54 @@
 package com.wyatt.myblog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*****************************
+     * 操作栏
+     * 1.如何使用
+     * 2.关系结构
+     * ******************************
+     * recycleView
+     * ******************************
+     * 使用greenDao存储数据
+     * ******************************
+     * 使用Sharepreference
+     * ******************************
+     * io存储
+     * ******************************
+     * 使用RxJava
+     * ******************************
+     * 使用Retrofit
+     * ******************************
+     * ******************************
+     * ******************************
+     * ******************************
+     * ******************************
+     * ******************************
+     * ******************************
+     * ******************************
+     * 构想：
+     *
+     * ******************************
+     *
+     */
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onCreateBlog() {
+        Intent intent = new Intent();
+        intent.setClass(this, CreateBlogActivity.class);
+        startActivity(intent);
+
     }
 }
